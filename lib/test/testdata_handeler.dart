688ec7197f9748a5de7fca8bc56ca8de44cc2b57
supunnilakshana <supunnilakshana1999@gmail.com>
2022-04-27 07:22:17 +0530

    animation_init

---
+++ lib/test/testdata_handeler.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:vinsartisanmarket/test/testmodel.dart';
+
+class TestDataHandeler {
+  Future<List<TestModel>> fetchTestModel() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+
+      return parsed.map<TestModel>((json) => TestModel.fromMap(json)).toList();
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+}
